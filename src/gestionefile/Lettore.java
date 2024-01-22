0d0f55a992678617c34517105813423e47982f80
MovingN <152166664+MovingN@users.noreply.github.com>
2024-01-22 20:46:39 +0100

    Update Lettore.java
    Esercizio 2

--- src/gestionefile/Lettore.java
+++ src/gestionefile/Lettore.java
@@ -23,16 +23,13 @@ public class Lettore extends Thread{
     public void leggi(){
         FileReader fr;
         int i; 
-        try { 
+        try (FileReader fr = new FileReader(nomeFile)){ 
             //1) apro il file
-            fr = new FileReader(nomeFile);
             //2) leggo carattere per carattere e lo stampo 
             while ((i=fr.read()) != -1)
                 System.out.print((char) i);
             
             System.out.print("\n\r");
-            //3) chiudo il file
-            fr.close();
         } catch (IOException ex) {
             System.err.println("Errore in lettura!");
         }
